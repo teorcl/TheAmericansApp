4d2b00697827ef38876590abbdd6cc247fce85af
Teo <teo.calle.lara@gmail.com>
2023-01-09 16:19:35 -0500

    Initial Commit

---
+++ TheAmericansApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheAmericansApp
+//
+//  Created by ADMIN on 9/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
